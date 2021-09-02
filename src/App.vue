9d078b09d5a608c8173871b88ac8bc71299c4cd3
Viktor Sudarinen <salveffy@gmail.com>
2021-09-02 17:28:47 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AnecdotCard msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AnecdotCard from './components/AnecdotCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AnecdotCard
   }
 }
 </script>
---
+++ src/components/AnecdotCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnecdotCard',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
